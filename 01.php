61d1843328145cd45574ce3f623870e85d076e3c
dacelibermane <dace.libermane@gmail.com>
2022-11-06 21:02:43 +0200

    user can add coins

--- 01.php
+++ 01.php
@@ -11,13 +11,13 @@ function createProducts(int $id, string $title, int $price): stdClass
 $products = [
     createProducts(0, 'Black Coffee', 120),
     createProducts(1, 'White Coffee', 130),
-    createProducts(2, 'Latte', 170),
+    createProducts(2, 'Latte', 500),
     createProducts(3, 'Cappuccino', 150)
 ];
 
 //Key is coin value and value is the amount of keys.
 $coins = [
-    200 => 50, 100 => 40, 50 => 60, 20 => 10, 10 => 40, 2 => 20, 1 => 30
+    200 => 50, 100 => 40, 50 => 0, 20 => 10, 10 => 40,5 => 10, 2 => 20, 1 => 30
 ];
 
 function formatMoney(int $amount, string $currency = '€'): string
@@ -28,16 +28,25 @@ function formatMoney(int $amount, string $currency = '€'): string
 echo "Welcome!\n";
 echo "Products Available: \n";
 foreach ($products as $product) {
-    echo "{$product->id}. {$product->title} - {$product->price}\n";
+    echo $product->id. ". " . $product->title . " - " . formatMoney($product->price). "\n";
 }
-$userSelection = (int)readline("<< \n");
+$userSelection = (int)readline("<< ");
 echo PHP_EOL;
 $selectedProduct = $products[$userSelection];
+echo "You selected - " . $selectedProduct->title . " " .formatMoney($product->price) ."\n";
 
+$insertedCoins = 0;
 
-echo "You selected - {$selectedProduct->title}.\n";
-$insertedCoins = readline("Please insert money: \n");
-echo PHP_EOL;
+while($insertedCoins < $selectedProduct->price) {
+    echo "You inserted " . formatmoney($insertedCoins)."\n";
+    $coinInput = (int)readline("\nPlease insert more coins: ");
+    if (!in_array($coinInput, array_keys($coins))) {
+        echo "Invalid coin!\n";
+    } else {
+        $insertedCoins += $coinInput;
+    }
+}
+
 $remainder = $insertedCoins - $selectedProduct->price;
 
 $coinsReturned = '';
@@ -47,11 +56,13 @@ foreach ($coins as $value => $amount) {
         break;
     }
     $times = intdiv($remainder, $value);
-    $remainder -= $value * $times;
-    $coinsReturned .= formatMoney($value);
-
+    if($times !== 0){
+        $remainder -= $value * $times;
+        $coinsReturned .= formatMoney($value);
+        $amount -= $times;
+    }
 }
 
-echo "Coins - " . $coinsReturned;
+echo "Coins returned - " . $coinsReturned;
 
 
